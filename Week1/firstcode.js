fa25808c25ef7cddb76f01f1c66f4095e191410b
uma89mahe <49181346+uma89mahe@users.noreply.github.com>
2025-10-16 09:39:28 +0530

    Add files via upload
    Day1 assignment

---
+++ Week1/firstcode.js
@@ -0,0 +1,13 @@
+var firstName = 'Umamaheswari'
+var companyName = 'Testleaf'
+var mobileNumber = 1234546789
+var isAutomation = true
+var hasPlaywright
+
+console.log("My Name is " +firstName +" ;which is of type: "+ typeof(firstName))
+console.log("company Name is " +companyName + " ;which is of type: "+ typeof(companyName))
+console.log("Sample Mobile number is " +mobileNumber + " ;which is of type: "+ typeof(mobileNumber))
+console.log("Is this automation class?: " +isAutomation+ " ;which is of type: "+ typeof(isAutomation))
+console.log("value of this variable: "+hasPlaywright+ " ;which is of type: "+ typeof(hasPlaywright))
+
+
