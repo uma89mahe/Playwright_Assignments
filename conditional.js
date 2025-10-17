0fe19636ea63bb83acd4bed940465c23e926ba95
uma89mahe <49181346+uma89mahe@users.noreply.github.com>
2025-10-17 09:57:20 +0530

    Add files via upload
    Day2 homework

---
+++ conditional.js
@@ -0,0 +1,10 @@
+let marks = 20
+if(marks>70){
+    console.log("marks secured greater than 70")
+}else if(marks>55){
+    console.log("marks secured greater than 55")
+}else if(marks>45){
+    console.log("marks secured greater than 45")
+}else{
+    console.log("marks secured less than 45")
+}
